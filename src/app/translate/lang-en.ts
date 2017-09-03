be835655797ba2001d254dd97c8d7d259411a521
bvattapparam <bijeshkumarvattapparambath@Bijeshkumars-MacBook-Pro.local>
2017-09-03 07:22:22 +0530

    Added new button labels
    Added new button labels

---
+++ src/app/translate/lang-en.ts
@@ -0,0 +1,66 @@
+export const LANG_EN_NAME = 'en';
+
+export const LANG_EN_TRANS = {
+    // DASHBOARD SECTION ...
+    'DASHBOARD_HEADER'      :   'Dashboard',
+    'BREADCRUM_DASHBOARD'   :   'Dashboard',
+
+    // FLAT MANAGER SECTION ...
+    'FLAT_MANAGER_HEADER'       :   'Flat Manager',
+    'BREADCRUM_FLAT_MANAGER'    :   'Flat Manager',
+
+    // PROPERTY TAX SECTION ...
+    'PROPERTYTAX_HEADER'        :   'Property Tax',
+    'BREADCRUM_PROPERTYTAX'     :   'Property Tax',
+
+    // FLAT PAYMENT SECTION ...
+    'FLAT_PAYMENT_HEADER'           :   'Flat Payment',
+    'BREADCRUM_FLAT_PAYMENT'        :   'Flat Payment',
+    'LABEL.PAIDDATE'                :   'Paid Date',
+    'LABEL.MODE'                    :   'Mode',
+    'LABEL.CHEQUENO'                :   'Cheque',
+    'LABEL.DDNO'                    :   'DD NO',
+    'LABEL.WIRENO'                  :   'Wire Transfer',
+    'LABEL.MCHNO'                   :   'MCH NO',
+    'LABEL.PURPOSE'                 :   'Purpose',
+    'LABEL.RECEIPT'                 :   'Receipt',
+    'LABEL.AMOUNT'                  :   'Amount',
+    'LABEL.CONTACTPERSON'           :   'Contact Person',
+    
+
+    // SIDE MENU SECTION ...
+    'SIDE_MENU_DASHBOARD_TOOLTIP'           :   'Dashboard',
+    'SIDE_MENU_FLAT_MANAGER_TOOLTIP'        :   'flast Manager',
+    'SIDE_MENU_CAR_MANAGER_TOOLTIP'         :   'Car Manager',
+    'SIDE_MENU_LOAN_MANAGER_TOOLTIP'        :   'Loan Manager',
+    'SIDE_MENU_GAS_MANAGER_TOOLTIP'         :   'Gas Manager',
+    'SIDE_MENU_INSURANCE_MANAGER_TOOLTIP'   :   'Insurance Manager',
+
+    // GENERIC ITEMS ...
+    'TOOLTIP_NOTIFICATION'      :   'Notification',
+    'TOOLTIP_TODO_ALERT'        :   'ToDo List',
+    'BUTTON_CANCEL'             :   'Cancel',
+    'BUTTON_SAVE'               :   'Save',
+    'BUTTON_EDIT'               :   "Edit",
+    'BUTTON_UPLOAD'             :   'Upload',
+    'LABEL.ACTION'              :   'Action',
+    'TBL_HEADER_TOTAL_AMOUNT'   :   'Total Amount: ',
+    'TBL_HEADER_TOTAL_RECORDS'  :   'Total Record: ',
+
+
+    //SCHEDULER SECTION ...
+    'SCHEDULER.CREATE.TITLE'            :   'Scheduler Title',
+    'SCHEDULER.CREATE.COLOR'            :   'Scheduler color',
+    'SCHEDULER.CREATE.START'            :   'Scheduler Start',
+    'SCHEDULER.CREATE.END'              :   'Scheduler End',
+    'SCHEDULER.CREATE.DESCRIPTION'      :   'Scheduler Description',
+
+    // HEADER SETTINGS MENU & SETTINGS SECTION ...
+    'SETTINGS_DROPMENU_SETTINGS'        :   'Settings',
+    'SETTINGS_DROPMENU_LANGUAGE'        :   'Change Language',
+    'SETTINGS_DROPMENU_PASSWORD'        :   'Password Reset',
+    'SETTINGS_LANGSETTING_HEADER'       :   'Language Setting',
+    'BREADCRUM_LANGSETTING'             :   'Language Setting'
+    
+
+};
